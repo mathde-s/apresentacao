2778f4b7f910622136b34113a2565748049c6509
matheus <matheus.pereira@zup.com.br>
2024-12-02 16:42:09 -0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        String nome = "matheus";
+        int idade = 19;
+        double altura = 1.69;
+        boolean matriculadoNoCurso = true;
+
+        System.out.println("meu nome é: " + nome);
+        System.out.println("minha idade é: " + idade);
+        System.out.println("minha altura é: " + altura);
+        System.out.println("matriculado: " + matriculadoNoCurso);
+
+    }
+}
